1a761ebcaa5207d1fc904ed88d56d27440ac7766
Axel Garcia Rosales <axel.garcia2992@alumnos.udg.mx>
2026-06-10 22:22:06 -0600

    Añadi menu de seleccion

--- Calculadora.c
+++ Calculadora.c
@@ -3,5 +3,13 @@
 int main() {
     int opcion;
     float num1, num2, resultado;
-
+    
+    // 1. Solicitar la operación
+    printf("--- CALCULADORA BASICA ---\n");
+    printf("1. Sumar\n");
+    printf("2. Restar\n");
+    printf("3. Multiplicar\n");
+    printf("4. Dividir\n");
+    printf("Seleccione una operacion (1-4): ");
+    scanf("%d", &opcion);
     
