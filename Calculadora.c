dd3d28a8e74718132415e3443ac2d7a0450a9582
a26110046-boop <a26110046@ceti.mx>
2026-06-10 21:58:53 -0600

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,37 +1,5 @@
 #include <stdio.h>
 
-/* Buena práctica 1: Constantes descriptivas */
-
-#define SUMA 1
-
-#define RESTA 2
-
-#define MULTIPLICACION 3
-
-#define DIVISION 4
-
-/* Buena práctica 2: Función con una sola responsabilidad */
-
-float realizarOperacion(float numero1, float numero2, int opcion) {
-
-    switch(opcion) {
-
-        case SUMA:
-            return numero1 + numero2;
-
-        case RESTA:
-            return numero1 - numero2;
-
-        case MULTIPLICACION:
-            return numero1 * numero2;
-
-        case DIVISION:
-            if(numero2 != 0)
-                return numero1 / numero2;
-            else {
-                printf("Error: No se puede dividir entre cero.\n");
-                return 0;
-            }
 
 
 
