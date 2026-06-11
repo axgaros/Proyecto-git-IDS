3cbbc0fb0d564d0a4d277a1452ac98113c29ad81
CasielBA <a26110043@ceti.mx>
2026-06-10 21:57:58 -0600

    Update 2 Calculadora.c
    implementacion de funciones con una sola responsabilidad

--- Calculadora.c
+++ Calculadora.c
@@ -17,6 +17,21 @@ float realizarOperacion(float numero1, float numero2, int opcion) {
     switch(opcion) {
 
         case SUMA:
+            return numero1 + numero2;
+
+        case RESTA:
+            return numero1 - numero2;
+
+        case MULTIPLICACION:
+            return numero1 * numero2;
+
+        case DIVISION:
+            if(numero2 != 0)
+                return numero1 / numero2;
+            else {
+                printf("Error: No se puede dividir entre cero.\n");
+                return 0;
+            }
 
 
 
