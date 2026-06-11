e4bbcf2df4c203700b33d70302346fb5642ca4ed
CasielBA <a26110043@ceti.mx>
2026-06-10 22:10:30 -0600

    Constantes

--- Calculadora.c
+++ Calculadora.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/* Buena práctica 1: Constantes descriptivas */
+#define SUMA 1
+#define RESTA 2
+#define MULTIPLICACION 3
+#define DIVISION 4
+
+/* Buena práctica 2: Función con una sola responsabilidad */
+float realizarOperacion(float numero1, float numero2, int opcion) {
+    switch(opcion) {
+        case SUMA:
+            return numero1 + numero2;
+
 
 
 
