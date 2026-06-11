3b3f0136201b749e0762e6a9cb042ca3ffc707c6
a26110046-boop <a26110046@ceti.mx>
2026-06-10 21:47:48 -0600

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(){
+/* Buena práctica 1: Constantes descriptivas */
+
+#define SUMA 1
+
+#define RESTA 2
+
+#define MULTIPLICACION 3
+
+#define DIVISION 4
+
+/* Buena práctica 2: Función con una sola responsabilidad */
+
+float realizarOperacion(float numero1, float numero2, int opcion) {
+
+    switch(opcion) {
+
+        case SUMA:
 
 
 
